b22399f1e9437464d89ce175e7c49635b932bded
GrigoriyNestsiarovich <79170403+GrigoriyNestsiarovich@users.noreply.github.com>
2021-04-12 20:33:49 +0300

    feat: add solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,26 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const numbers = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let sum = 0;
+      if (i !== 0 && j !== 0 && matrix[i - 1][j - 1] === true) sum++;
+      if (i !== 0 && matrix[i - 1][j] === true) sum++;
+      if (i !== 0 && matrix[i - 1][j + 1] === true) sum++;
+      if (j !== 0 && matrix[i][j - 1] === true) sum++;
+      if (matrix[i][j + 1] === true) sum++;
+      if (i !== (matrix.length - 1) && j !== 0 && matrix[i + 1][j - 1] === true) sum++;
+      if (i !== (matrix.length - 1) && matrix[i + 1][j] === true) sum++;
+      if (i !== (matrix.length - 1) && j !== (matrix[0].length - 1)
+      && matrix[i + 1][j + 1] === true) sum++;
+      numbers.push(sum);
+    }
+    res.push(numbers);
+  }
+  return res;
 }
 
 module.exports = minesweeper;
